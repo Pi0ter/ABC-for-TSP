88a489e2be7b2edce671a64e43a1facddf9cd469
Pi0tr <48968610+Pi0ter@users.noreply.github.com>
2021-02-13 18:33:27 +0100

    Final?

--- ABC for TSP.cpp
+++ ABC for TSP.cpp
@@ -8,76 +8,97 @@
 #include <random>
 #include <chrono>
 
+
 using namespace std;
 
 struct Punkt; 
 class Bee;   
 
+//! Lista punktów wczytanych przez program
 std::vector<Punkt> punkty;
+//! Lista pszczół w roju
 std::vector<Bee> pszczoly;
+//! Lista punktów w kolejności odpowiadającej najlepszemu znanemu rozwiązaniu
 vector<int> najlepszeRozwiazanie;
+//! Długość najkrótszego znanego rozwiązania
 float najkrotszaDroga = (float)INT_MAX;
+//! Tablica dwu wymiarowa w której zawarte są dystanse między poszczególnymi punktami
 std::vector<std::vector<float>>  tablicaDystansow;
-
+//! Nazwa pliku w którym znajduje się lista punktów
+std::string nazwaPliku;
+//! Ilość pszczół w roju
+int iloscPszczol;
+//! Procent pszczół z zachowaniem obserwatora
 int procentZwiadowcow;
+//! Procent pszczół z zachowaniem zbieracza
 int procentZbieraczy;
-// procentZwiadowcow + procent zbieraczy = 100
+//! Procent pszczół z zachowaniem zwiadowcy
 int procentSkautow;
-//procentSkautow to osobny procent potrzebny do przetwarzania rozwiązań 
 
+//! Limit prób mutacji rozwiązania jakie podejmują pszczoły zbieracze
 int limitIteracji;
+
+//! Limit cykli tańca
 int cykle;
 
 
-/**************************************************************/
 
+//! Struktura każdego punktu w trasie
 struct Punkt {
 public:
     float X;
     float Y;
 };
 
+//! Klasa pszczoły
 class Bee {
 public:
-    char rola = ' '; // Przyporządkowana rola pszczoły[F,O,S]
-    std::vector<int> trasa; // Każda pszczoła zna wszystkie punkty do przebycia 
+    //! Przyporządkowana rola (zachowanie) pszczoły możliwe dane[F,O,S]
+    char rola = ' '; 
+    //! Każda pszczoła zna wszystkie punkty  we własnej trasie i przechowuje je w tej zmiennej
+    std::vector<int> trasa; 
+    //! Dystans posiadanej przez pszczołę trasy
     double dystans =0;
-    int iterator=0; // Licznik kolejnych iteracji w których pszczoła wskazywała na to samo rozwiązanie
-    
+    //! Licznik kolejnych iteracji w których pszczoła wskazywała na to samo rozwiązanie
+    int iterator=0;     
 };
+
+//! Funkcja wypisująca informacje o konkretnej pszczole
 void infoPszczola(Bee a);
-/**
-Prawdopodobnie do struktury lub tabeli struktur
+
+//! Funkcja wczytująca plik z punktami
+/*!
+ \param nazwaPliku nazwa pliku z którego wczytywana jest liczba punktów.
 */
-void wczytywaniePunktow(std::string a) {   
-    float x = 0, y = 0, d = 0;
-    std::fstream plik("/ali535.txt",ios::in);
-   
+void wczytywaniePunktow(std::string nazwaPliku) {
+
+    std::ifstream plik(nazwaPliku);
     if (!plik) {
-        std::cout << std::endl << "Brak pliku";
+        cout << endl << "Brak pliku";
         throw("Brak pliku");
-        return;
     }
-   
-    if (plik.is_open()) {
-        while (plik >>d>> x >> y) {
-            std::cout << std::endl << d;
-            Punkt tempPunkt;
-            tempPunkt.X = x;
-            tempPunkt.Y = y;
-            punkty.push_back(tempPunkt);
-        }
+    float x, y,d;
+
+    while (plik >>d>> x >> y) {
+        Punkt tempPunkt;
+        tempPunkt.X = x;
+        tempPunkt.Y = y;
+        punkty.push_back(tempPunkt);
     }
 
-    //test
+ /*   
     for (auto x : punkty) {
         cout << x.X << " " << x.Y << endl;
-    }
-    plik.close();
+    }*/
+
 }
 
-/**
-Dystans między punktami, pierwsza zmienna to punkt w kolejności na globalnej liście, druga to drugi punkt
+
+//! Funkcja obliczająca dystans pomiędzy dwoma punktami
+/*!
+ \param a id punkt na globalnej liście
+ \param b id punktu na globalnej liście
+ \param pnkty tablica punktów wczytanych przez program
 */
 float dystansMiedzyPunktami(int a,int b, std::vector<Punkt> pnkty) {
     float ax, ay, bx, by, dystans;
@@ -93,9 +114,7 @@ float dystansMiedzyPunktami(int a,int b, std::vector<Punkt> pnkty) {
     return dystans;
 }
 
-/**
-Oblicza dystans pomiędzy poszczególnymi punktami w tabeli i zpisuje je do tabeli
-*/
+//! Oblicza dystans pomiędzy wszytskimi punktami w tabeli i zapisuje je do tabeli dystansów
 void poszczegolneDystanse() {
     int liczbaWierszy = punkty.size();
     cout << endl << "Liczba wszystkch punktow wprowadzonych do systemu: " << liczbaWierszy;
@@ -117,7 +136,7 @@ void poszczegolneDystanse() {
     }
 
 }
-
+//! Wypisuje wczytane punktu oraz tablicę dystansów.
 void wypiszPunktyIDystanse() {
 
     cout << endl << "Wprowadzone punkty do systemu i ich numeracja";
@@ -135,7 +154,7 @@ void wypiszPunktyIDystanse() {
 
 
 }
-
+//! Wypisuje aktualnie posiadane trasy przez każdą pszczołę w roju.
 void wypiszTrasyroju() {
 
     cout << endl << "Trasy roju" << endl;
@@ -145,15 +164,11 @@ void wypiszTrasyroju() {
             cout << " " << y;
         }
         cout << " - " << x.rola << x.dystans;
-
     }
-
 }
 
 
-/**
-Oblicza dystans rozwiązania jakie prezentuje dana pszczoła
-*/
+//! Oblicza dystans rozwiązania jakie posiada dana pszczoła.
 float dlugoscSciezki(vector<int> a) {
     float dlugosc = 0;
     int punktA, punktB;
@@ -170,11 +185,8 @@ float dlugoscSciezki(vector<int> a) {
     return dlugosc;
 }
 
-/**
-Inicjalizacja roju pszczół
-*/
-void inicjalizacjaRoju(int iloscPszczol) {
-    //inicjalizacja
+//! Inicjalizacja roju pszczół za pomocą losowej ścieżki.
+void inicjalizacjaRoju(int iloscPszczol) {    
     for (int i = 0; i < iloscPszczol; i++) {
         Bee temp;
         for (int i = 0; i < punkty.size(); i++) {
@@ -189,35 +201,29 @@ void inicjalizacjaRoju(int iloscPszczol) {
 }
 
 
-/**
-Przyporządkowanie podziału na rolę pszczół (zwiadowca, zbieracz i skaut?)
-*/
+//! Wprowadza podział roju na role według parametrów  (obserwator, zbieracz i zwiadowca).
 void podzialRoju() {
     int liczbaPszczol = pszczoly.size();
     float lZwiad = float(procentZwiadowcow) / 100;
     float lZbier = float(procentZwiadowcow) / 100;
-    //cout << lZbier << " " << lZwiad;
+    
     lZwiad = floor(lZwiad * liczbaPszczol);
     lZbier = floor(lZbier * liczbaPszczol);
-    //cout <<endl<< lZbier << " " << lZwiad;
+
     for (int i = 0; i < lZwiad; i++) {
         pszczoly[i].rola = 'O'; 
-        //cout << endl << pszczoly[i].rola;
     }
 
     for (int i = lZwiad; i < liczbaPszczol; i++) {
-        pszczoly[i].rola = 'F'; //Forager / Zbieracz 
+        pszczoly[i].rola = 'F';
         unsigned seed = chrono::system_clock::now().time_since_epoch().count();
         shuffle(pszczoly[i].trasa.begin(), pszczoly[i].trasa.end(), default_random_engine(seed));        
        
-        pszczoly[i].dystans = dlugoscSciezki(pszczoly[i].trasa);
-        //cout <<endl<< pszczoly[i].dystans;
+        pszczoly[i].dystans = dlugoscSciezki(pszczoly[i].trasa);        
     }    
 }
 
-/**
-Mutowanie ścieżek po przez zmianę kolejności odwiedzanych punktów (zwykła zamiana wybranych PUNKTOW w ścieżce a=b b=a)
-*/
+//! Mutowanie ścieżek po przez zamianę kolejności dwóch losowych punktów.
 vector<int> mutujSciezke(Bee *a) {
     random_device rd;
     mt19937 mt(rd());
@@ -226,7 +232,9 @@ vector<int> mutujSciezke(Bee *a) {
     int temp = 0;
     int numerPunktuDoZamiany = 0;
     numerPunktuDoZamiany = distrib(mt);
-  
+    /*if (numerPunktuDoZamiany > 4) {
+        throw("XD");
+    }*/
     vector<int> zmutowana;
 
     for (auto x : a->trasa) {
@@ -240,12 +248,10 @@ vector<int> mutujSciezke(Bee *a) {
     return zmutowana;
 }
 
-/**
-    Przy pomocy mutujSciezke()#{? nie wiem czy nie dało by się tego w jednej funkcji}
-    próbuje znaleźć lepszą drogę po przez ponowne obliczenie dystansu pomiędzy punktami w nowej kolejności
-    Jeśli dystans jest większy niż poprzedni przez określoną ilość iteracji pszczoły, pszczoła z zbieracza zmienia się w zwiadowcę. [brak postępu]
-
-    ? nie wiem czy to dobra nazwa funkcji - może się mylić z funkcją szukaj
+//! Eksploatacja znanych rozwiązań przez pszczoły zbieracze.
+/*!   
+    Dopóki dana pszczoła nie osiągnie limitu iteracji będzie starała się po przez mutowanie ścieżki odnaleźć lepszą.
+    Jeżeli jej to się nie uda, pszczoła zostaje zwiadowcą i porzuca dotychczasowe rozwiązanie.       
 */
 void zwiad(Bee *a) {
 
@@ -256,59 +262,52 @@ void zwiad(Bee *a) {
         a->trasa = zmutowana;
         a->dystans = nowaDlugosc;
         a->iterator = 0;
+
     }
     else {
         if (a->iterator >= limitIteracji) {
             a->rola = 'S';
         }
         else {
-            a->iterator++;
+            a->iterator++ ; 
         }
     }
 }
 
-/**
-    Pszczoła zwiadowca bierze nową losową (chyba z innej funkcji) trasę i zmienia się w pszczołę zbieracza.
-*/
+//! Generacja nowej losowej trasy dla pszczoły
 void szukajNowejSciezki(Bee a){
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     shuffle(a.trasa.begin(), a.trasa.end(), default_random_engine(seed));
     a.dystans = dlugoscSciezki(a.trasa);
-    a.iterator = 0;    
-    a.rola = 'F';
+    a.iterator = 0;
     
+    a.rola = 'F';    
 }
 
-/**
-    Wybiera losowe trasy dla pszczół zwiadowców
-    Określana jest najlepsza dotychczasowa trasa
-    Pszczoły z najgorszymi trasami zmieniają się w zwiadowców
 
-
-    Potrzebna będzie możliwość przekazanie parametrów przekonującej pszczoły do globalnej(?) zmiennej jako nalepszego rozwiązania 
-    > dystans najlepszego rozwiązania [chyba żeby obliczać go gdy będzie potrzebny na bierząco osobną funkcją]
-    > ścieżka tej trasy między poszczególnymi punktami
-*/
-
+//! Część funkcji sortującej 
 bool sortowanie(vector<float> a,vector<float> b) {
     return a[1] > b[1];
 }
 
+//! Funkcja przydziela najlepszą znaną trasę pszczole obserwatorowi w celu eksploracji.
  vector<int> kopiujNajlepszaTrase(Bee a) {
     vector<int> temp;
 
     for (auto x : a.trasa) {
         temp.push_back(x);
     }
-
-    /*for (int i = 0; i < a->trasa.size(); i++) {
-        temp.push_back(a->trasa[i]);
-    }*/
-
+       
     return temp;
 }
 
+ //! Główna pętla programu ralizująca komunikację.
+ /*!
+    W tej funkcji każda pszczoła z zachowaniem zbieracz eksploatuje własne rozwiązanie, czyli po  przez mutację próbuje znaleźć lepsze.
+    Jeśli jej się to uda i osiągnięte roziązanie jest lepsze od globalnie najlepszego to zostaje ono podmienione.
 
+    Wszystkie dotychczasowe rozwiązania z aktualnego cyklu są zapamiętywane a następnie ustalony procent najgorszych rozwiązań staje się zwiadowcami. 
+ */
 void taniec(){
     vector<vector<float>> dotychczasoweRozwiazania;
     
@@ -317,10 +316,8 @@ void taniec(){
         //infoPszczola(pszczoly[i]);
         if (pszczoly[i].rola == 'F') {
             zwiad(&pszczoly[i]);
-            if (najkrotszaDroga > pszczoly[i].dystans) {
-
-                //najlepszeRozwiazanie = pszczoly[i].trasa;
-                najlepszeRozwiazanie  =  kopiujNajlepszaTrase(pszczoly[i]);
+            if (najkrotszaDroga > pszczoly[i].dystans) {                
+               najlepszeRozwiazanie  =  kopiujNajlepszaTrase(pszczoly[i]);
 
                 najkrotszaDroga = pszczoly[i].dystans;
 
@@ -329,21 +326,26 @@ void taniec(){
             }
 
             dotychczasoweRozwiazania.push_back(vector<float>(0));
+
             dotychczasoweRozwiazania[dotychczasoweRozwiazania.size() - 1].push_back(i);            
-            dotychczasoweRozwiazania[dotychczasoweRozwiazania.size() - 1].push_back(pszczoly[i].dystans);            
+            dotychczasoweRozwiazania[dotychczasoweRozwiazania.size() - 1].push_back(pszczoly[i].dystans);
+            
         
         }
         else {
             if (pszczoly[i].rola == 'S') {
-                szukajNowejSciezki(pszczoly[i]);                
+                szukajNowejSciezki(pszczoly[i]);
+                
             }
         }        
-    }    
+    }
+    
 
     sort(dotychczasoweRozwiazania.begin(), dotychczasoweRozwiazania.end(), sortowanie);
 
     float lSkautow = (float)procentSkautow / 100;
-    lSkautow = floor(lSkautow * dotychczasoweRozwiazania.size());       
+    lSkautow = floor(lSkautow * dotychczasoweRozwiazania.size());    
+    
     
     for (int i = 0; i < (int)lSkautow; i++) {
         int idPszcz = dotychczasoweRozwiazania[i][0];
@@ -352,30 +354,26 @@ void taniec(){
 
 }
 
-
+//! Kopiowanie trasy pszczoły
 vector<int> kopiujTrase(vector<int> a) {
     vector<int> temp;
 
     for (auto x : a) {
         temp.push_back(x);
     }
-    //for (int i = 0; i < a->trasa.size(); i++) {
-    //    temp.push_back(a->trasa[i]);
-    //}
-
+  
     return temp;
 }
 
 
-/**
-    Zbiera pszczoły zbieraczy aby "pracowały" nad najlepszym (najbardziej obiecującym) aktualnie rozwiązaniem
-    Nazwa robocza
-*/
+//! Funkcja zwołuje wszystkie pszczoły obserwatorów, aby te zaczęły eksplorację najlepszego znanego rozwiązania
 void wezwanieDoNajlepszegoRozwiazania(vector<Bee> pszczoly) {
     for (auto x : pszczoly) {
         if (x.rola == 'O') {
-            //x.trasa = najlepszeRozwiazanie;
-            x.trasa = kopiujTrase(najlepszeRozwiazanie);
+            
+            x.trasa = kopiujTrase(najlepszeRozwiazanie);            
+            
+
             x.trasa = mutujSciezke(&x);
             x.dystans = dlugoscSciezki(x.trasa);
             if (x.dystans < dlugoscSciezki(najlepszeRozwiazanie)) {
@@ -392,9 +390,11 @@ void wezwanieDoNajlepszegoRozwiazania(vector<Bee> pszczoly) {
         }        
 
     }
+
+
 }
 
-
+//! Funkcja udostępnia informację o pszczole
 void infoPszczola(Bee a) {
     cout << endl << "> Rola: " << a.rola << " Dystans: " << a.dystans << " iteracja: " << a.iterator<<" Trasa: [";
     for (auto x : a.trasa) {
@@ -406,68 +406,93 @@ void infoPszczola(Bee a) {
 
 int main()
 {
-    std::string nazwaPliku= "bier127.txt";
+
+    //std::string nazwaPliku = "ali535.txt";
+    //procentZbieraczy = 40; // w % 
+    //procentZwiadowcow = 60; 
+    //procentSkautow = 20;
+
+    //limitIteracji = 100; // Limit mutacji trasy dla pojedyńczej pszczoły
+    //cykle = 500;        // Limit cykli algorytmu 
 
 
-    procentZbieraczy = 30; // w % 
-    procentZwiadowcow = 70; 
-    procentSkautow = 20;
 
-    limitIteracji = 100; // Limit mutacji trasy dla pojedyńczej pszczoły
-    cykle = 500;        // Limit cykli algorytmu 
+    // iloscPszczol = 2000;
 
-
-    int iloscPszczol = 1000;
-
-
-    clock_t czasInicjacji = clock();
-    wczytywaniePunktow(nazwaPliku);
-    poszczegolneDystanse();
-    wypiszPunktyIDystanse();
-    czasInicjacji = clock() - czasInicjacji;
-
-    cout<< endl << "  Inicjacja zajela: "<< (float)czasInicjacji/CLOCKS_PER_SEC<<" sekund"<<
-        endl<<"Czesc wlasciwa: " << endl << endl;
-
-    clock_t czas = clock();
-
-
-    inicjalizacjaRoju(iloscPszczol);
-    podzialRoju();
-    //wypiszTrasyroju();
-    for (int i = 0; i < cykle; i++) {
-        taniec();
-        wezwanieDoNajlepszegoRozwiazania(pszczoly);
-        cout << ".";
-    }
-    czas = clock() - czas;
-
-    cout << endl << "Rozwiazanie znaleziono w " << (float)czas/CLOCKS_PER_SEC << " sekund(y)";
-
-    cout << endl << "Najlepsze rozwiazanie: " << endl;    
     
-    cout << "[ ";
-    for (auto x : najlepszeRozwiazanie) {
-            cout << " > " << x;
-    }
-    cout << "] Dystans: " << dlugoscSciezki(najlepszeRozwiazanie);
+    while (1) {
+
+        std::cout << "Pliku do zaladowania (w formacie nazwa.txt): ";
+        getline(std::cin, nazwaPliku);
+
+        std::cout << "Liczba pszczol w roju: ";
+        std::cin >> iloscPszczol;
+        std::cout<<std::endl;
+
+        std::cout << "Liczba zbieraczy w %: ";
+        std::cin >> procentZbieraczy;
+        std::cout << std::endl;
+        
+        std::cout << "Liczba obserwatorow w %: ";
+        std::cin >> procentZwiadowcow;
+        std::cout << std::endl;
+
+        if ((100 - procentZbieraczy) < procentZwiadowcow) {
+            procentZwiadowcow = 100 - procentZbieraczy;
+        }
 
 
-    cout << endl  << endl;
+        std::cout << "Liczba zwiadowcow w %: ";
+        std::cin >> procentSkautow;
+        std::cout << std::endl;
+
+        std::cout << "Limit iteracji: ";
+        std::cin >> limitIteracji;
+        std::cout << std::endl;
+
+        std::cout << "Liczba cykli algorytmu: ";
+        std::cin >> cykle;
+        std::cout << std::endl;
 
 
-    /*for (auto x : pszczoly) {
-        cout << x.iterator<<" ";
-    }*/
+        clock_t czasInicjacji = clock();
+        wczytywaniePunktow(nazwaPliku);
+        poszczegolneDystanse();
+        wypiszPunktyIDystanse();
+        czasInicjacji = clock() - czasInicjacji;
 
 
-    //for (auto x : pszczoly) {
-    //    cout << endl;
-    //    for (auto y : x.trasa) {
-    //        cout << " " << y;
-    //    }
-    //}
+        cout<< endl << "  Inicjacja zajela: "<< (float)czasInicjacji/CLOCKS_PER_SEC<<" sekund"<<
+            endl<<"Czesc wlasciwa: " << endl << endl;
 
-    //cout<<endl<<dlugoscSciezki(pszczoly[1]);
+        clock_t czas = clock();
+
+
+        inicjalizacjaRoju(iloscPszczol);
+        podzialRoju();
+        //wypiszTrasyroju();
+        for (int i = 0; i < cykle; i++) {
+            taniec();          
+
+            wezwanieDoNajlepszegoRozwiazania(pszczoly);
+            cout << ".";
+        }
+        czas = clock() - czas;
+
+        cout << endl << "Rozwiazanie znaleziono w " << (float)czas/CLOCKS_PER_SEC << " sekund(y)";
+
+        cout << endl << "Najlepsze rozwiazanie: " << endl;    
     
+        cout << "[ ";
+        for (auto x : najlepszeRozwiazanie) {
+                cout << " > " << x;
+        }
+        cout << "] Dystans: " << dlugoscSciezki(najlepszeRozwiazanie);
+
+
+        cout << endl  << endl;
+
+        system("pause");
+    }
+      
 }
